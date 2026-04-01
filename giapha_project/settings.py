6cea1ccd930a9b69cfe47ba1743d60b94334e7bd
Hac Mieu <hacmieu@users.noreply.github.com>
2026-04-01 16:43:55 +0700

    Fix static files 404 by adding WhiteNoise middleware
    - Add whitenoise.middleware.WhiteNoiseMiddleware after SecurityMiddleware
    - Use CompressedManifestStaticFilesStorage for production static files
    - Add whitenoise to requirements.txt
    - Fixes DRF CSS/JS files returning 404 (MIME type errors in browser)
    - All static files now served correctly by gunicorn via WhiteNoise

--- giapha_project/settings.py
+++ giapha_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,6 +99,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (User uploads)
 MEDIA_URL = 'media/'
